9369c422faf6a72ddd6dfd3aa31aa65395edd49a
Bmeza05 <brianmeza952@gmail.com>
2021-05-26 19:42:51 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var accept = document.getElementById("cookie")
+function removecookie(){
+    accept.replaceWith('')
+}
+var element = document.getElementById("city")
+function dispAlert(){
+    alert("Loading weather report...")
+}
+var element2 = document.getElementById("city2")
+function dispAlert2(){
+    alert("Loading weather report...")
+}
+var element3 = document.getElementById("city3")
+function dispAlert3(){
+    alert("Loading weather report...")
+}
